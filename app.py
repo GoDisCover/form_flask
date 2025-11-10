6ea10bcd5c802645043c93bc241fa5988fabe571
GoDisCover <jessenco122@gmail.com>
2025-11-10 20:46:49 +0700

    upload kntl bkn uplaud

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from forms.NewMahasiswa import NewMahasiswa
 
 
 app = Flask(__name__)
-UPLOUD_FOLDER = "uplouds"
+UPLOAD_FOLDER = "uploads"
 app.config["SECRET_KEY"] = "SECRET_KEY"
-app.config["UPLOUD_FILE"] = UPLOUD_FOLDER
-app.config["MAX_CONTENT_LENGHT"] = 1 * 1024 * 1024
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 1 * 1024 * 1024
 list_mahasiswa = []
 
 @app.route("/")
@@ -26,7 +26,7 @@ def add():
         npm = form.npm.data
         profile_picture = form.profile_picture.data
         file_name = secure_filename(profile_picture.filename)
-        save_dir = os.path.join(UPLOUD_FOLDER,"media")
+        save_dir = os.path.join(UPLOAD_FOLDER,"media")
         os.makedirs(save_dir,exist_ok=True)
         profile_picture.save(os.path.join(save_dir,file_name))
         list_mahasiswa.insert(0, {
@@ -38,9 +38,9 @@ def add():
 
     return render_template("add_new_mahasiswa.html", title="Add New Mahasiswa", form=form)
 
-@app.route("/uploads/<path>:filename")
-def uploud_file(filename):
-    return send_from_directory(app.config["UPLOUD_FILE"],filename)
+@app.route("/uploads/<path:filename>")
+def upload_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"],filename)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
